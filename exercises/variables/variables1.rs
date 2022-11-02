ed71d9fc38fc8de659a32bbdc67612996e63566c
thy1037 <thy1037@qq.com>
2022-11-02 21:18:12 +0800

    day2

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -1,10 +1,13 @@
 // variables1.rs
 // Make me compile!
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
+// 1. 完成 rustlings 编程练习
+// 2. 阅读 rustlings hint
+// 3. 阅读 The Book
+// 4. 答疑
 
-// I AM NOT DONE
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
 }
