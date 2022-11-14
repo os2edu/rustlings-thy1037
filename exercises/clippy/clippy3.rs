70d5d70824edca2d3be0eb7ef1421e7c66ca8633
thy1037 <thy1037@qq.com>
2022-11-14 22:44:41 +0800

    fixed run clippy3

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -7,7 +7,7 @@ fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
         // my_option.unwrap();
-        panic!();
+        println!("panic");
     }
 
     let my_arr = &[
